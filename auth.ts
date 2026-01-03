f2d7c2bd592cfbfa3b75c60693a9506adef6cd56
ashutoshbetchoo <64080592+ashutoshbetchoo@users.noreply.github.com>
2026-01-03 19:14:16 +0400

    Implement OAuth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,59 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import type { IAccount } from "./database/account.model";
+import { api } from "./lib/api";
+import type { ActionResponse } from "./types/global";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
+  callbacks: {
+    async session({ session, token }) {
+      session.user.id = token.sub as string;
+      return session;
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        const { data: existingAccount, success } =
+          (await api.accounts.getByProvider(
+            account.type === "credentials"
+              ? // biome-ignore lint/style/noNonNullAssertion: <email will always be present for credentials>
+                token.email!
+              : account.providerAccountId,
+          )) as ActionResponse<IAccount>;
+
+        if (!success || !existingAccount) return token;
+
+        const userId = existingAccount.userId;
+
+        if (userId) token.sub = userId.toString();
+      }
+
+      return token;
+    },
+    async signIn({ user, profile, account }) {
+      if (account?.provider === "credentials") return true;
+
+      if (!account || !user) return false;
+
+      const userInfo = {
+        name: user.name,
+        email: user.email,
+        image: user.image,
+        username:
+          account.provider === "github"
+            ? (profile?.login as string)
+            : (user.name?.toLowerCase() as string),
+      };
+
+      const { success } = (await api.auth.oAuthSignIn({
+        user: userInfo as SignInWithOAuthParams["user"],
+        provider: account.provider as "github",
+        providerAccountId: account.providerAccountId as string,
+      })) as ActionResponse;
+
+      if (!success) return false;
+
+      return true;
+    },
+  },
 });
